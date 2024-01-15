28b4bac880c18c258dbc2eb3c18610d3387760a9
Gupta <DZ508YA@vw.vwgroup.com>
2024-01-15 12:45:44 +0530

    firrst

---
+++ coursera/spring-boot-server/src/test/java/in/co/devendra/spring/datajpa/SpringBootDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package in.co.devendra.spring.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
